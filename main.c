b71affcb832710f4a9506df4da909e3aa7759511
Owen Anderson <resistor@resistor-desktop.(none)>
2009-12-13 02:29:42 -0800

    Add some comments. ;-)

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 #include "pack.h"
 #include "lodepng.h"
 
+/* img_t - A simple wrapper for image data, filename, and width/height */
 typedef struct Img {
     unsigned char* pixels;
     unsigned char* filename;
     unsigned w, h;
 } img_t;
 
+/* image_comparator - a qsort-compatible comparison function for img_t*'s
+ * Sorts first by width, then height */
 int image_comparator(const void* a, const void* b) {
   img_t* aimg = *(img_t**)a;
   img_t* bimg = *(img_t**)b;
@@ -28,23 +31,17 @@ int image_comparator(const void* a, const void* b) {
   }
 }
 
-unsigned int nlpo2(register unsigned int x) {
-        if (!(x & (x-1))) return x;
-        x |= (x >> 1);
-        x |= (x >> 2);
-        x |= (x >> 4);
-        x |= (x >> 8);
-        x |= (x >> 16);
-        return(x+1);
-}
-
-
+/* Usage: spritepack outfile infile1 infile2 ... */
 int main(int argc, char** argv) {
+  /* Init the PNG decoder */
   LodePNG_Decoder decoder;
   LodePNG_Decoder_init(&decoder);
   
+  /* Buffer to hold img_t*'s for the input images */
   img_t** images = malloc((argc-2) * sizeof(img_t*));
   
+  /* Loop over the program arguments, decoding input PNGs and
+   * creating img_t*'s for them */
   unsigned i  = 0;
   for (i = 0; i < argc-2; ++i) {
     unsigned char *buffer, *image;
@@ -62,15 +59,21 @@ int main(int argc, char** argv) {
   
   LodePNG_Decoder_cleanup(&decoder);
   
+  /* Sort the images, because pack_rects expects input in sorted order */
   qsort(images, argc-2, sizeof(img_t*), image_comparator);
 
-
+  /* Create a proxy array of rectangles, which pack_rects will use as input.
+   * Note that the indexing matches the indexing into the images array for
+   * easy correspondence. */
   unsigned* rects = malloc(2 * (argc-2) * sizeof(unsigned));
   for (i = 0; i < argc-2; ++i) {
     rects[2*i] = images[i]->w;
     rects[2*i+1] = images[i]->h;
   }
   
+  /* Attempt to find the best packing by choosing a base width,
+   * generating a packing, and repeating with larger widths until
+   * the total megapixels of the image start going back up. */
   unsigned max_x = 512;
   unsigned max_y = 513;
   double MP = 0.0;
@@ -91,13 +94,18 @@ int main(int argc, char** argv) {
     ret = new_ret;
   }
   
+  /* Spew input image offsets */
   for (i = 0; i < argc-2; ++i) {
     printf("file %s @ X: %d, Y: %d\n",
            images[i]->filename, ret[2*i], ret[2*i+1]);
   }
   
+  /* Allocate the output image */
   unsigned char* out_image = calloc(4 * max_x * max_y, sizeof(unsigned char));
   
+  /* Blit each input image into the output image at the offset determined
+   * by the packing.  This is a bit complicated, since the decoded pixels
+   * are indexed from the top-left, while the packing uses bottom-left indexing */
   for (i = 0; i < argc-2; ++i) {
     unsigned off_x = ret[2*i];
     unsigned off_y = max_y - ret[2*i+1] - images[i]->h;
@@ -121,6 +129,7 @@ int main(int argc, char** argv) {
     }
   }
   
+  /* Encode and output the output image */
   unsigned char* buffer;
   size_t buffersize;
   LodePNG_Encoder encoder;
